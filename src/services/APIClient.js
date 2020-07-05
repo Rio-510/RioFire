9b55794348133b508442ed3e033be22f473c5962
sadachanpi <poniponi.063@gmail.com>
2020-07-05 23:17:14 +0900

    initial commit

---
+++ src/services/APIClient.js
@@ -0,0 +1,83 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey || '',
+  authDomain: process.env.authDomain || '',
+  databaseURL: process.env.databaseURL || '',
+  projectId: process.env.projectId || '',
+  storageBucket: process.env.storageBucket || '',
+  messagingSenderId: process.env.messagingSenderId || '',
+  appId: process.env.appId || '',
+  measurementId: process.env.measurementId || ''
+};
+
+firebase.initializeApp(firebaseConfig)
+
+const db = firebase.firestore()
+
+export const getBlogs = async () => {
+  const querySnapshot = await db.collection("blogs").get()
+  const data = []
+  querySnapshot.forEach((doc) => {
+    data.push({ id: doc.id, title: doc.data().title })
+  })
+  return data
+}
+
+export const createBlog = (blog) => db.collection("blogs").add({ title: blog })
+
+export const deleteBlog = (blogId) => db.collection('blogs').doc(blogId).delete()
+
+
+class APIClient {
+  constructor() {
+    this.blogs = [
+      {title:"おはようございます。",body:"今日もいい天気ですね。",created:new Date()},
+      ];
+    this.skills = [
+      "Django",
+      "Python",
+      "法学",
+      "営業スキル",
+    ]
+  }
+
+  getSkills(){
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(this.skills);
+      }, 1000);
+    });
+
+  }
+
+  getBlogs() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(this.blogs);
+      }, 1000);
+    });
+  }
+
+  createBlog(blog) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        this.blogs.push(blog);
+        resolve();
+      }, 1000);
+    });
+  }
+
+  deleteBlog(blog) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        this.blogs = this.blogs.filter((value) => value != blog);
+        resolve();
+      }, 1000);
+    });
+  }
+}
+
+export default APIClient;
+
+
